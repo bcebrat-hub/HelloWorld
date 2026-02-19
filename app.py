465ef69299c8bb250bbb84f56835803d0c632d86
bencebrat <bcebrat@terpmail.umd.edu>
2026-02-19 14:48:31 -0500

    added my "About me" html template to app file

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ def hello_world():
 @app.route('/hello')
 def hello():
     return "Hello World from Ben Cebrat! This is my first HTML page."
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("About.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
