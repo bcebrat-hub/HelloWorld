45b3eed4f4827370decc6891f94f4fed3f7e8eff
bencebrat <bcebrat@terpmail.umd.edu>
2026-02-19 19:31:45 -0500

    fixed code to make about.html work

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return "Hello World from Ben Cebrat! I am adding my first code change."
-
-@app.route('/hello')
-def hello():
-    return "Hello World from Ben Cebrat! This is my first HTML page."
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -17,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("About.html")
 
+@app.route("/hello")
+def hello():
+    return "Hello World from Ben Cebrat! This is my first HTML page."
+
+@app.route("/hi")
+def hello_world():
+    return "Hello World from Ben Cebrat! I am adding my first code change."
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
